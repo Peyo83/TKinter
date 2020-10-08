1bf8143936d95a70220a848d88478fa31635c1b3
Peyo83 <jpvase@gmail.com>
2020-10-08 20:22:10 +0200

    marcos

---
+++ 05-marcos.py
@@ -0,0 +1,67 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.title('Ventana de Peyo')
+ventana.geometry('700x700')
+
+marco_padre = Frame(ventana, width=250, height=250)
+
+marco_padre.config(
+    bg='lightblue',
+    bd=5,
+    relief=SOLID
+)
+
+marco_padre.pack(side=BOTTOM, fill=X, expand=YES)
+
+marco = Frame(marco_padre, width=250, height=250)
+
+marco.config(
+    bg='red',
+    bd=5,
+    relief=RAISED
+)
+
+marco.pack(side=LEFT, anchor=SW)
+
+marco = Frame(marco_padre, width=250, height=250)
+
+marco.config(
+    bg='green',
+    bd=5,
+    relief=RAISED
+)
+
+marco.pack(side=RIGHT, anchor=SE)
+
+marco_padre = Frame(ventana, width=250, height=250)
+
+marco_padre.config(
+    bg='lightblue',
+    bd=5,
+    relief=SOLID 
+)
+
+marco = Frame(marco_padre, width=250, height=250)
+
+marco.config(
+    bg='yellow',
+    bd=5,
+    relief=RAISED
+)
+
+marco.pack(side=LEFT, anchor=NW)
+
+marco = Frame(marco_padre, width=250, height=250)
+
+marco.config(
+    bg='blue',
+    bd=5,
+    relief=RAISED
+)
+
+marco.pack(side=RIGHT, anchor=NE)
+
+marco_padre.pack(side=TOP, fill=X, expand=YES)
+
+ventana.mainloop()
